302cac9ba2fbd025a30cebdec9458d3bba55aa00
Pablo Sousa <pablossilva.005@gmail.com>
2021-10-15 20:29:22 -0300

    Update processa_envio.php

--- app_send_mail-private/processa_envio.php
+++ app_send_mail-private/processa_envio.php
@@ -51,14 +51,14 @@
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'cefet2020tcc@gmail.com';                     //SMTP username
-        $mail->Password   = 'tcccefet@2020';                               //SMTP password
+        $mail->Username   = 'youremail@mail.com';                     //SMTP username
+        $mail->Password   = 'password';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         
 
         //Recipients
-        $mail->setFrom('pablocefet@gmail.com');
+        $mail->setFrom('email@gmail.com');
         $mail->addAddress($mensagem->__get('para'));     //Add a recipient
         //$mail->addReplyTo('info@example.com', 'Information');
         //$mail->addCC('cc@example.com');
